b9c96fad20ccafd606583fb431d92ceff3bf3357
Cristian Stiven Puenguenan <styven21121@gmail.com>
2025-05-03 13:20:59 -0500

    feat: prettier

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { Client } = require('@notionhq/client');
-const { from, concatMap, tap, mergeMap, filter, } = require('rxjs');
-require('dotenv').config()
+const { Client } = require("@notionhq/client");
+const { from, concatMap, tap, mergeMap, filter } = require("rxjs");
+require("dotenv").config();
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
@@ -8,26 +8,30 @@ const notion = new Client({
 
 const DATABASE_ID = process.env.DATABASE_ID;
 
-const  updateDates = (done,keys) => {
+const updateDates = (done, keys) => {
   return from(
     notion.databases.query({
       database_id: DATABASE_ID,
-    })
+    }),
   ).pipe(
     mergeMap((response) => from(response.results)),
-    concatMap(page => from(notion.pages.update({
-      page_id: page.id,
-      properties: {
-        [done]: {
-          checkbox: false,
-        }
-      }
-    }))),
+    concatMap((page) =>
+      from(
+        notion.pages.update({
+          page_id: page.id,
+          properties: {
+            [done]: {
+              checkbox: false,
+            },
+          },
+        }),
+      ),
+    ),
     concatMap((page) =>
       from(keys).pipe(
         filter((key) => {
           const property = page.properties[key];
-          return property?.type === 'date' && property.date?.start;
+          return property?.type === "date" && property.date?.start;
         }),
         concatMap((key) => {
           const pageId = page.id;
@@ -46,21 +50,20 @@ const  updateDates = (done,keys) => {
             notion.pages.update({
               page_id: pageId,
               properties: { [key]: { date } },
-            })
+            }),
           ).pipe(
             tap(() =>
-              console.log(`Actualizado: ${pageId} → ${newDate.toDateString()}`)
-            )
+              console.log(`Actualizado: ${pageId} → ${newDate.toDateString()}`),
+            ),
           );
-        })
-      )
-    )
+        }),
+      ),
+    ),
   );
-}
+};
 
 // Ejecutar y manejar errores
-updateDates('Done', ['Start Date', 'End Date']).subscribe({
-  complete: () => console.log('Proceso completado'),
-  error: (err) => console.error('Error:', err),
+updateDates("Done", ["Start Date", "End Date"]).subscribe({
+  complete: () => console.log("Proceso completado"),
+  error: (err) => console.error("Error:", err),
 });
-  
