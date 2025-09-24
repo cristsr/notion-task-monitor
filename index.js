a5a0d14b74783fd4e3149460213bfe048aff3a31
Cristian Stiven Puenguenan <styven21121@gmail.com>
2025-09-24 17:02:39 -0500

    feat: use luxon to better integration with dates

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { Client } = require("@notionhq/client");
-const { from, concatMap, tap, mergeMap, filter } = require("rxjs");
-require("dotenv").config();
+const { Client } = require('@notionhq/client');
+const { from, concatMap, tap, mergeMap, filter } = require('rxjs');
+const luxon = require('luxon');
+require('dotenv').config();
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
@@ -31,29 +32,36 @@ const updateDates = (done, keys) => {
       from(keys).pipe(
         filter((key) => {
           const property = page.properties[key];
-          return property?.type === "date" && property.date?.start;
+          return property?.type === 'date' && property.date?.start;
         }),
         concatMap((key) => {
           const pageId = page.id;
           const property = page.properties[key];
 
-          const currentDate = new Date(property.date.start);
-          const newDate = new Date(currentDate);
-          newDate.setDate(newDate.getDate() + 1);
+          const prevDate = luxon.DateTime.fromISO(property.date.start);
 
-          const date = {
-            start: newDate.toISOString(),
-          };
+          const nextDate = luxon.DateTime.now().set({
+            hour: prevDate.hour,
+            minute: prevDate.minute,
+            second: prevDate.second,
+            millisecond: prevDate.millisecond,
+          });
 
           // Actualizar la página en Notion
           return from(
             notion.pages.update({
               page_id: pageId,
-              properties: { [key]: { date } },
+              properties: {
+                [key]: {
+                  date: {
+                    start: nextDate.toISO(),
+                  },
+                },
+              },
             }),
           ).pipe(
             tap(() =>
-              console.log(`Actualizado: ${pageId} → ${newDate.toDateString()}`),
+              console.log(`Actualizado: ${pageId} → ${nextDate.toISO()}`),
             ),
           );
         }),
@@ -63,7 +71,7 @@ const updateDates = (done, keys) => {
 };
 
 // Ejecutar y manejar errores
-updateDates("Done", ["Start Date", "End Date"]).subscribe({
-  complete: () => console.log("Proceso completado"),
-  error: (err) => console.error("Error:", err),
+updateDates('Done', ['Start Date', 'End Date']).subscribe({
+  complete: () => console.log('Proceso completado'),
+  error: (err) => console.error('Error:', err),
 });
