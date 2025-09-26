6164ce6ba35c1d3bb8c06c83204ef1a2b351a04d
Cristian Stiven Puenguenan <styven21121@gmail.com>
2025-09-25 23:13:35 -0500

    fix: use time zone for dates to avoid inconsistencies

--- apps/notifier/src/notion/repositories/notion.repository.ts
+++ apps/notifier/src/notion/repositories/notion.repository.ts
@@ -1,11 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Cache } from '@nestjs/cache-manager';
 import { Page } from '../dto/notion.dto';
 import { DateTime } from 'luxon';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class NotionRepository {
-  constructor(private cache: Cache) {}
+  private readonly logger = new Logger(NotionRepository.name);
+
+  constructor(
+    private readonly cache: Cache,
+    private readonly config: ConfigService,
+  ) {}
 
   async setPages(data: Page[]): Promise<void> {
     await this.cache.set('pages', data);
@@ -23,13 +29,17 @@ export class NotionRepository {
       return;
     }
 
-    const now = DateTime.local({ zone: 'America/Bogota' }).set({
+    const zone = this.config.get('TIME_ZONE');
+
+    const now = DateTime.local({ zone }).set({
       second: 0,
       millisecond: 0,
     });
 
+    this.logger.log(`Attempting to notify at ${now.toISO()}`);
+
     return pages.find((page) => {
-      const startDate = DateTime.fromISO(page.startDate);
+      const startDate = DateTime.fromISO(page.startDate).setZone(zone);
 
       const minus = startDate.minute - 0;
 
