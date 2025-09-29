c53ce025b7cf22a20a642a6773d375b1b8167385
Cristian Stiven Puenguenan <styven21121@gmail.com>
2025-09-28 22:41:02 -0500

    feat: remove redundant logs

--- apps/notifier/src/task/schedulers/task.scheduler.ts
+++ apps/notifier/src/task/schedulers/task.scheduler.ts
@@ -1,16 +1,13 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { TaskService } from '../services/task.service';
 import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class TaskScheduler {
-  private readonly logger = new Logger(TaskScheduler.name);
-
   constructor(private readonly appService: TaskService) {}
 
   @Cron(CronExpression.EVERY_MINUTE)
   async notify(): Promise<void> {
-    this.logger.log('Notification scheduler started');
     await this.appService.notifyTask();
   }
 }
