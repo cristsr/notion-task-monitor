8001aeb21cf0b77d6de599b7ab7f149478b62b4d
Cristian Stiven Puenguenan <styven21121@gmail.com>
2026-01-27 21:26:30 -0500

    feat: rename Notion route and mark as public
    - Updated the Notion route from `events` to `webhook`.
    - Added `Public` decorator to allow bypassing authentication for the route.

--- apps/notifier/src/shared/infrastructure/adapters/http/notion.controller.ts
+++ apps/notifier/src/shared/infrastructure/adapters/http/notion.controller.ts
@@ -1,13 +1,15 @@
 import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { EventEmitterPort } from '../../../application/ports';
 import { NotionEventInput } from '../../dtos';
+import { Public } from '../../decorators';
 
 @Controller('notion')
 export class NotionController {
   private readonly logger = new Logger(NotionController.name);
   constructor(private readonly eventEmitter: EventEmitterPort) {}
 
-  @Post('events')
+  @Public()
+  @Post('webhook')
   onEvent(@Body() event: NotionEventInput) {
     this.logger.log({
       message: 'Notion event received',
